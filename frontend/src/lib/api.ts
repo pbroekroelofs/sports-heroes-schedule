f257a186d841f8a3849cb16cc42621038c181956
Peter Broekroelofs <pbroekroelofs@gmail.com>
2026-02-28 21:29:58 +0100

    Fix: Uint8Array type for VAPID applicationServerKey

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -7,7 +7,9 @@ function urlBase64ToUint8Array(base64String: string): Uint8Array {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
   const rawData = atob(base64);
-  return Uint8Array.from([...rawData].map((char) => char.charCodeAt(0)));
+  const output = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; i++) output[i] = rawData.charCodeAt(i);
+  return output;
 }
 
 const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8080';
